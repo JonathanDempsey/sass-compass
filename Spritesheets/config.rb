ecd00da47bc5c29e732cb05f6b7701932fc3597e
JonathanDempsey <jonathanedempsey@gmail.com>
2017-02-09 16:12:34 +0000

    Final Project
    Sass Compass final project

---
+++ Spritesheets/config.rb
@@ -0,0 +1,13 @@
+# Require any additional compass plugins here.
+
+# Set this to the root of your project when deployed:
+http_path = "/"
+css_dir = "css"
+sass_dir = "sass"
+images_dir = "img"
+fonts_dir = "fonts"
+javascripts_dir = "js"
+# http_images_path = "http://my-site.com/img"
+# http_generated_images_path = "http://assets.my-prj.com/img"
+# environment = :production
+relative_assets = true
